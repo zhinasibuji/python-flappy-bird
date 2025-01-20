ef48a093af992c7858f795c5c4b81f81c4a899ff
Administrator <xyj784516912@gmail.com>
2025-01-20 23:56:39 +0800

    changes

--- src/main.py
+++ src/main.py
@@ -4,10 +4,11 @@ import random
 from pathlib import Path
 from itertools import cycle
 
+
 class Pipe(pygame.sprite.Sprite):
     def __init__(self, x: int, y: int, upper: bool) -> None:
         super().__init__(pipes)
-        image = pygame.image.load(PIPE_PATH)
+        image = sprites.load("pipe")
         if upper:
             self.image = pygame.transform.flip(image, False, True)
         else:
@@ -26,7 +27,7 @@ class Pipe(pygame.sprite.Sprite):
 class Bird(pygame.sprite.Sprite):
     def __init__(self) -> None:
         ls = []
-        for entry in IMAGE_PATH.glob("yellowbird*.png"):
+        for entry in IMAGE_PATH.glob("bird*.png"):
             ls.append(pygame.image.load(entry).convert())
         self.images = cycle(ls)
         self.image = next(self.images)
@@ -51,11 +52,16 @@ class Bird(pygame.sprite.Sprite):
             self.image = next(self.images)
             self.frame_count = 0
         self.frame_count += 1
+    def die(self) -> None:
+        global gameover
+        gameover = True
+        sound.play("hit")
+
 
 
 class Background(pygame.sprite.Sprite):
     def __init__(self) -> None:
-        self.image = pygame.image.load(BACKGROUND_PATH).convert()
+        self.image = sprites.load("background")
 
     def update(self) -> None:
         screen.blit(self.image, (0, 0))
@@ -63,7 +69,7 @@ class Background(pygame.sprite.Sprite):
 
 class Ground(pygame.sprite.Sprite):
     def __init__(self) -> None:
-        self.image = pygame.image.load(GROUND_PATH).convert()
+        self.image = sprites.load("base")
         self.rect = self.image.get_rect()
         self.rect.y = 400
 
@@ -92,22 +98,43 @@ class Pipes(pygame.sprite.Group):
         self.frame_count += 1
 
 
+class Sound:
+    def __init__(self) -> None:
+        self.content = {}
+        for entry in SOUND_PATH.glob("*.ogg"):
+            self.content[entry.stem] = pygame.mixer.Sound(entry)
+
+    def play(self, name) -> None:
+        self.content[name].play()
+
+class Sprites:
+    def __init__(self) -> None:
+        self.content = {}
+        for entry in IMAGE_PATH.glob("*.png"):
+            self.content[entry.stem] = pygame.image.load(entry).convert()
+
+    def load(self, name) -> pygame.Surface:
+        return self.content[name]
+
+
+
+
 
 BLACK = (0, 0, 0)
 IMAGE_PATH = Path("assets", "sprites")
-BACKGROUND_PATH = Path("assets", "sprites", "background-day.png")
-GROUND_PATH = Path("assets", "sprites", "base.png")
-PIPE_PATH = Path("assets", "sprites", "pipe-green.png")
+SOUND_PATH = Path("assets", "audio")
 GRAVITY = 0.2
 
 pygame.init()
 screen = pygame.display.set_mode((288, 512))
 pygame.display.set_caption("flappy bird")
 clock = pygame.time.Clock()
+sprites = Sprites()
 bird = Bird()
 backround = Background()
 ground = Ground()
 pipes = Pipes()
+sound = Sound()
 
 while True:
     for event in pygame.event.get():
@@ -115,6 +142,7 @@ while True:
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
+                sound.play("wing")
                 bird.jump()
 
     backround.update()
@@ -123,11 +151,10 @@ while True:
     bird.update()
 
     if pygame.sprite.collide_rect(bird, ground):
-        print("gameover")
-
+        bird.die()
     for pipe in pipes:
         if pygame.sprite.collide_rect(bird, pipe):
-            print("gameover")
+            bird.die()
 
     pygame.display.flip()
     clock.tick(60)
